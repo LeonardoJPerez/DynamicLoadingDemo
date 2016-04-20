5f83109d00e06b8818d731b09dca8e1eb7b2b7ea
Leonardo Perez <leonardop@overgroup.com>
2016-04-20 12:19:26 -0400

    Initial Commit

---
+++ DynamicLoadingDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace DynamicLoadingDemo
+{
+    public interface IService
+    {
+        void Execute();
+    }
+
+    public class A : IService
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Executing Class A");
+        }
+    }
+
+    public class B : IService
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Executing Class B");
+        }
+    }
+
+    public class Program
+    {
+        public static IService GetNewService(string serviceName)
+        {
+            var type = Type.GetType(serviceName, true);
+            var newInstance = Activator.CreateInstance(type);
+            return newInstance as IService;
+        }
+
+        private static void Main(string[] args)
+        {
+            GetNewService("DynamicLoadingDemo.A").Execute();
+            GetNewService("DynamicLoadingDemo.B").Execute();
+
+            Console.ReadLine();
+        }
+    }
+}
